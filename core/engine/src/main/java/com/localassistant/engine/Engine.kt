fde481827866eef44b52571cc52c19c183f488c8
Danmoreng <sebastianerlhof@yahoo.de>
2025-05-04 17:34:43 +0200

    new core module

---
+++ core/engine/src/main/java/com/localassistant/engine/Engine.kt
@@ -0,0 +1,4 @@
+package com.localassistant.engine
+
+class Engine {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "LocalAssistant"
 include(":app")
+include(":core:engine")
